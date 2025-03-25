f28b2b4750862ddee52b6961c9b5c69eda36e4de
Jayshree2004 <jayshree.r2022cse@sece.ac.in>
2025-03-25 12:02:47 +0530

    initial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,22 @@
+import React from "react";
+import "../App.css"; 
+
+
 function Header() {
     return (
         <header>
             <h1>User Management</h1>
+            <form>
+                <div>
+                    <label>Name: </label>
+                    <input type="text" name="name" placeholder="Enter your name" />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input type="email" name="email" placeholder="Enter your email" />
+                </div>
+                <button type="submit">Add</button>
+            </form>
         </header>
     );
 }
